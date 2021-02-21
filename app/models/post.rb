11a9175189d151ae015d97c2f0422e37e815063b
Alex Cline <rev.a.r.cline@gmail.com>
2021-02-20 19:44:13 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,23 @@
 class Post < ActiveRecord::Base
-
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
+
+  before_validation :make_title_case
+
+  before_save :email_author_about_post
 
   private
 
   def is_title_case
-    if title.split.any?{|w|w[0].upcase != w[0]}
-      errors.add(:title, "Title must be in title case")
-    end
+    errors.add(:title, 'Title must be in title case') if title.split.any? { |w| w[0].upcase != w[0] }
+  end
+
+  def email_author_about_post
+    # not implemented
+    # look up actionmailer later
   end
 
   def make_title_case
-    self.title = self.title.titlecase
+    self.title = title.titlecase
   end
 end
